b0f5481232002df10136b60ab870f18ab1d8426e
Cameron Howieson <cameron.howieson@gmail.com>
2013-11-09 17:42:19 -0500

    Deleting old files

--- public/js/ext/jquery.placeholder.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-	$(document).on('change keydown keypress input', '*[data-placeholder]', function() {
-		if (this.textContent) {
-			this.setAttribute('data-placeholder-content', 'true');
-		} else {
-			this.removeAttribute('data-placeholder-content');
-		}
-	});
-})(jQuery);
